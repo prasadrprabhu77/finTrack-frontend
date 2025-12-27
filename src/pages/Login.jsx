e84b81f36168224fd56d228350d009f68f8bdfee
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2025-12-27 18:02:21 +0530

    feat: complete login page UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,80 @@
+import { Wallet } from "lucide-react";
+import ThemeToggle from "../components/ThemeToggle";
+
 const Login = () => {
   return (
-    <div>
-      Login Page
+    <div className="relative min-h-screen">
+      {/* Dark Mode Toggle – Top Right */}
+      <div className="absolute top-4 right-4 z-10">
+        <ThemeToggle />
+      </div>
+    <div className="min-h-screen flex items-center justify-center bg-bgLight dark:bg-bgDark px-4">
+      <div className="w-full max-w-md bg-white dark:bg-cardDark rounded-xl shadow-md p-6">
+        
+        {/* Logo */}
+        <div className="flex items-center justify-center gap-2 mb-6">
+          <Wallet className="text-primary" size={28} />
+          <h1 className="text-2xl font-bold">FinTrack</h1>
+        </div>
+
+        {/* Heading */}
+        <h2 className="text-xl font-semibold text-center mb-1">
+          Welcome back
+        </h2>
+        <p className="text-center text-slate-500 dark:text-slate-400 mb-6">
+          Login to your account
+        </p>
+
+        {/* Form */}
+        <form className="space-y-4">
+          {/* Email */}
+          <div>
+            <label className="block text-sm font-medium mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="you@example.com"
+              className="w-full px-3 py-2 border rounded-lg outline-none
+              border-slate-300 dark:border-slate-600
+              bg-white dark:bg-slate-800
+              focus:ring-2 focus:ring-primary"
+              />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="block text-sm font-medium mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              className="w-full px-3 py-2 border rounded-lg outline-none
+              border-slate-300 dark:border-slate-600
+              bg-white dark:bg-slate-800
+              focus:ring-2 focus:ring-primary"
+              />
+          </div>
+
+          {/* Button */}
+          <button
+            type="button"
+            className="w-full bg-primary text-white py-2 rounded-lg font-medium hover:opacity-90 transition"
+            >
+            Login
+          </button>
+        </form>
+
+        {/* Footer */}
+        <p className="text-center text-sm text-slate-500 dark:text-slate-400 mt-6">
+          Don’t have an account?{" "}
+          <span className="text-primary font-medium cursor-pointer">
+            Sign up
+          </span>
+        </p>
+      </div>
+    </div>
     </div>
   );
 };
